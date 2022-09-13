da914847c2dd7005e0ddde40dbb973a25dbcbf6e
altansuvd <altansuvd2524@gmail.com>
2022-09-13 14:46:24 +0800

    Rolling the dice action

--- app.js
+++ app.js
@@ -7,19 +7,21 @@ var score = [0, 0];
 // Create a variable for saving scores for each round
 var roundScore = 0;
 
-// Create a variable for dice (randomise between 1 - 6)
-var dice = Math.floor(Math.random() * 6) + 1;
-
 /* <div class="player-score" id="score-0">43</div> */
 // document.querySelector("#score-0").textContent = dice;
 // document.querySelector("#score-0").innerHTML = "<em>Yes!</em>";
 
 // Preparing to start the program
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-console.log("Dice : " + dice);
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
