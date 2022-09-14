a8fb82e1cc53fad18b980796c3ca92a1f10c2eb6
altansuvd <altansuvd2524@gmail.com>
2022-09-14 14:44:29 +0800

    New game button is ready

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
-// Create a variable for each players (Player 1 => 0; Player 2 => 1)
-var activePlayer = 0;
+// Global variable for each players...
+var activePlayer;
+// ...scores for each players
+var score;
+// ...current scores that is saved by each player
+var roundScore;
 
-// Create a variable for saving scores
-var score = [0, 0];
-
-// Create a variable for saving scores for each round
-var roundScore = 0;
-
-/* <div class="player-score" id="score-0">43</div> */
-// document.querySelector("#score-0").textContent = dice;
-// document.querySelector("#score-0").innerHTML = "<em>Yes!</em>";
-
-// Preparing to start the program
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-1").textContent = 0;
-
+// This variable shows the dice image from DOM
 var diceDom = document.querySelector(".dice");
-diceDom.style.display = "none";
+
+//Start the game
+initGame;
+
+// function for restarting the game
+function initGame() {
+  // Create a variable for each players (Player 1 => 0; Player 2 => 1)
+  activePlayer = 0;
+
+  // Create a variable for saving scores
+  score = [0, 0];
+
+  // Create a variable for saving scores for each round
+  roundScore = 0;
+
+  /* <div class="player-score" id="score-0">43</div> */
+  // document.querySelector("#score-0").textContent = dice;
+  // document.querySelector("#score-0").innerHTML = "<em>Yes!</em>";
+
+  // Preparing to start the program
+  document.getElementById("score-0").textContent = 0;
+  document.getElementById("score-1").textContent = 0;
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-1").textContent = 0;
+
+  // Show the players' name
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+
+  diceDom.style.display = "none";
+}
 
 // addEventListener for ROLL DICE button
 document.querySelector(".btn-roll").addEventListener("click", function () {
@@ -71,7 +98,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
   }
 });
 
-// This function switches player's turn
+// function for switching the players' turn
 function switchToNextPlayer() {
   // Make the player's score back to 0
   roundScore = 0;
@@ -93,3 +120,4 @@ function switchToNextPlayer() {
 }
 
 // addEventListener for NEW GAME button
+document.querySelector(".btn-new").addEventListener("click", initGame);
